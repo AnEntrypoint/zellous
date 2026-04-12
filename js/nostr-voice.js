cc66a3baa816b10d1e60265416578f9c62b78b8f
lanmower <almagestfraternite@gmail.com>
2026-04-12 02:07:58 +0200

    Add TURN servers to nostr-voice ICE config for cross-network audio
    STUN alone fails for peers behind symmetric NAT (most home routers).
    Add openrelay.metered.ca TURN servers (UDP/TCP/TLS) so peers on
    different networks can relay audio when direct P2P is blocked.
    Also sync js/ and docs/ _iceServers to same 4 STUN + 4 TURN list.

--- js/nostr-voice.js
+++ js/nostr-voice.js
@@ -80,6 +80,10 @@ var nostrVoice = {
     {urls:'stun:stun1.l.google.com:19302'},
     {urls:'stun:stun.cloudflare.com:3478'},
     {urls:'stun:stun.nextcloud.com:443'},
+    {urls:'turn:openrelay.metered.ca:80',username:'openrelayproject',credential:'openrelayproject'},
+    {urls:'turn:openrelay.metered.ca:443',username:'openrelayproject',credential:'openrelayproject'},
+    {urls:'turn:openrelay.metered.ca:443?transport=tcp',username:'openrelayproject',credential:'openrelayproject'},
+    {urls:'turns:openrelay.metered.ca:443',username:'openrelayproject',credential:'openrelayproject'},
   ],
 
   _maybeConnect(peerPubkey) {
