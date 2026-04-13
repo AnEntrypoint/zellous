299e05bab8eb320c11f298352f819edd38744a9d
lanmower <almagestfraternite@gmail.com>
2026-04-13 14:35:25 +0200

    Remove duplicate vendor/ folder, keep docs/vendor/ only

--- vendor/htm.js
+++
@@ -1,5 +0,0 @@
-import { h, render, Component } from 'preact';
-import { html } from './htm-core.mjs';
-const boundHtml = html.bind(h);
-export { h, render, Component, boundHtml as html };
-export default boundHtml;
